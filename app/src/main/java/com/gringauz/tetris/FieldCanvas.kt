fd64433109192c99c09dc68cf130f53bb5ecfa7a
Alexander Gringauz <gringauz@yandex-team.ru>
2019-06-08 19:19:44 +0300

    tetromino colors

--- app/src/main/java/com/gringauz/tetris/FieldCanvas.kt
+++ app/src/main/java/com/gringauz/tetris/FieldCanvas.kt
@@ -3,6 +3,8 @@ package com.gringauz.tetris
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.support.annotation.ColorRes
+import android.support.v4.content.res.ResourcesCompat
 import android.util.AttributeSet
 import android.view.View
 import com.gringauz.tetris.core.FIELD_HEIGHT
@@ -24,6 +26,17 @@ class FieldCanvas: View {
     private val cellWidthPx = context.resources.getDimensionPixelSize(R.dimen.width_field) / FIELD_WIDTH
     private val cellHeightPx = context.resources.getDimensionPixelSize(R.dimen.height_field) / FIELD_HEIGHT
 
+    private val colors = hashMapOf(
+        Pair(TetrominoType.I, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_i, null)),
+        Pair(TetrominoType.L, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_l, null)),
+        Pair(TetrominoType.J, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_j, null)),
+        Pair(TetrominoType.O, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_o, null)),
+        Pair(TetrominoType.S, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_s, null)),
+        Pair(TetrominoType.T, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_t, null)),
+        Pair(TetrominoType.Z, ResourcesCompat.getColor(context.resources, R.color.color_tetromino_z, null))
+
+    )
+
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
@@ -35,7 +48,7 @@ class FieldCanvas: View {
             val i = index / FIELD_WIDTH
             val j = index - i * FIELD_WIDTH
             tetrominoType?.let { squareType ->
-                paint.color = color(squareType)
+                paint.color = colors[squareType]!!
                 canvas!!.drawRect(
                     (j * cellWidthPx).toFloat(),
                     (i * cellHeightPx).toFloat(),
@@ -46,6 +59,4 @@ class FieldCanvas: View {
             }
         }
     }
-
-    private fun color(tetrominoType: TetrominoType) = R.color.color_tetromino_all
 }
